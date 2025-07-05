b93a3ab26813ec7a87891947a6984e359e400a26
Dev-shamoo <s-aliyu@leadway-group.com>
2025-07-05 15:32:04 +0100

    fixed the test errors

--- src/utils/initialize.ts
+++ src/utils/initialize.ts
@@ -1,39 +1,45 @@
-import express, { Express } from "express"
-import { facebookStrategy, googleStrategy } from './passport';
-import routes, { loadRoutes } from 'src/routes/index';
+import express, { Express } from "express";
+import { facebookStrategy, googleStrategy } from "./passport";
+import routes, { loadRoutes } from "src/routes/index";
 
 import { ErrorHandler } from "./request-handlers";
-import cors from 'cors';
-import { env } from './helpers';
-import logger from 'pino-http';
-import methodOverride from 'method-override';
-import passport from 'passport';
-import path from 'path';
+import cors from "cors";
+import { env } from "./helpers";
+import logger from "pino-http";
+import methodOverride from "method-override";
+import passport from "passport";
+import path from "path";
 
 export const initialize = (app: Express) => {
-    // Parse application/x-www-form-urlencoded (for non-multipart forms)
-    app.use(express.urlencoded({ extended: true }));
+  // Parse application/x-www-form-urlencoded (for non-multipart forms)
+  app.use(express.urlencoded({ extended: true }));
 
-    // Method Override 
-    app.use(methodOverride('X-HTTP-Method'))
+  // Method Override
+  app.use(methodOverride("X-HTTP-Method"));
 
-    // Route And Cors
-    loadRoutes(path.resolve(__dirname, '../routes'));
-    app.use(cors());
-    app.use(routes);
+  // Route And Cors
+  loadRoutes(path.resolve(__dirname, "../routes"));
+  app.use(cors());
+  app.use(routes);
 
-    // Passport
-    passport.use(googleStrategy())
-    passport.use(facebookStrategy())
+  // Passport
+  const google = googleStrategy();
+  if (google) {
+    passport.use(google);
+  }
+  const facebook = facebookStrategy();
+  if (facebook) {
+    passport.use(facebook);
+  }
 
-    // Initialize 
-    app.use(passport.initialize());
+  // Initialize
+  app.use(passport.initialize());
 
-    // Error Handler
-    app.use(ErrorHandler)
+  // Error Handler
+  app.use(ErrorHandler);
 
-    // Logger
-    if (env('NODE_ENV') !== 'test') {
-        app.use(logger())
-    }
-}
+  // Logger
+  if (env("NODE_ENV") !== "test") {
+    app.use(logger());
+  }
+};
--- src/utils/passport.ts
+++ src/utils/passport.ts
@@ -8,71 +8,97 @@ import argon2 from "argon2";
 const prisma = new PrismaClient();
 
 export const googleStrategy = () => {
-    return new GoogleStrategy({
-        clientID: env('GOOGLE_CLIENT_ID'),
-        clientSecret: env('GOOGLE_CLIENT_SECRET'),
-        callbackURL: env('GOOGLE_CALLBACK'),
-        scope: ['profile']
+  const clientID = env("GOOGLE_CLIENT_ID");
+  const clientSecret = env("GOOGLE_CLIENT_SECRET");
+  const callbackURL = env("GOOGLE_CALLBACK");
+
+  if (!clientID || !clientSecret || !callbackURL) {
+    if (process.env.NODE_ENV === "test") {
+      // In test mode, skip strategy initialization
+      return null;
+    }
+    throw new Error("Missing Google OAuth environment variables");
+  }
+
+  return new GoogleStrategy(
+    {
+      clientID,
+      clientSecret,
+      callbackURL,
+      scope: ["profile"],
     },
-        async function (accessToken, refreshToken, profile, cb) {
-            try {
-                const email = profile.emails?.find(e => !!e.value)?.value!
-                const user = await prisma.user.findFirst({
-                    where: { googleId: profile.id },
-                    include: { curator: true }
-                })
-                if (user) {
-                    return cb(undefined, user!);
-                }
-                const cUser = await prisma.user.create({
-                    data: {
-                        firstName: profile.name?.givenName ?? '',
-                        lastName: profile.name?.familyName ?? '',
-                        email: email ?? `${profile.id}@gmail.com`,
-                        password: await argon2.hash(secureOtp(32)),
-                        googleId: profile.id,
-                    },
-                    include: { curator: true }
-                })
-                return cb(undefined, cUser);
-            } catch (error) {
-                return cb(error);
-            }
+    async function (accessToken, refreshToken, profile, cb) {
+      try {
+        const email = profile.emails?.find((e) => !!e.value)?.value!;
+        const user = await prisma.user.findFirst({
+          where: { googleId: profile.id },
+          include: { curator: true },
+        });
+        if (user) {
+          return cb(undefined, user!);
         }
-    )
-}
+        const cUser = await prisma.user.create({
+          data: {
+            firstName: profile.name?.givenName ?? "",
+            lastName: profile.name?.familyName ?? "",
+            email: email ?? `${profile.id}@gmail.com`,
+            password: await argon2.hash(secureOtp(32)),
+            googleId: profile.id,
+          },
+          include: { curator: true },
+        });
+        return cb(undefined, cUser);
+      } catch (error) {
+        return cb(error);
+      }
+    }
+  );
+};
 
 export const facebookStrategy = () => {
-    return new FacebookStrategy({
-        clientID: env('FACEBOOK_CLIENT_ID'),
-        clientSecret: env('FACEBOOK_CLIENT_SECRET'),
-        callbackURL: env('FACEBOOK_CALLBACK'),
-        profileFields: ['id', 'displayName', 'photos', 'email'],
+  const clientID = env("FACEBOOK_CLIENT_ID");
+  const clientSecret = env("FACEBOOK_CLIENT_SECRET");
+  const callbackURL = env("FACEBOOK_CALLBACK");
+
+  if (!clientID || !clientSecret || !callbackURL) {
+    if (process.env.NODE_ENV === "test") {
+      // In test mode, skip strategy initialization
+      return null;
+    }
+    throw new Error("Missing Facebook OAuth environment variables");
+  }
+
+  return new FacebookStrategy(
+    {
+      clientID,
+      clientSecret,
+      callbackURL,
+      profileFields: ["id", "displayName", "photos", "email"],
     },
-        async function (accessToken, refreshToken, profile, cb) {
-            try {
-                const email = profile.emails?.find(e => !!e.value)?.value!
-                const user = await prisma.user.findFirst({
-                    where: { facebookId: profile.id },
-                    include: { curator: true }
-                })
-                if (user) {
-                    return cb(undefined, user!);
-                }
-                const cUser = await prisma.user.create({
-                    data: {
-                        firstName: profile.name?.givenName ?? '',
-                        lastName: profile.name?.familyName ?? '',
-                        email: email ?? `${profile.id}@facebook.com`,
-                        password: await argon2.hash(secureOtp(32)),
-                        facebookId: profile.id,
-                    },
-                    include: { curator: true }
-                })
-                return cb(undefined, cUser);
-            } catch (error) {
-                return cb(error);
-            }
+    async function (accessToken, refreshToken, profile, cb) {
+      try {
+        const email = profile.emails?.find((e) => !!e.value)?.value!;
+        const user = await prisma.user.findFirst({
+          where: { facebookId: profile.id },
+          include: { curator: true },
+        });
+        if (user) {
+          return cb(undefined, user!);
         }
-    )
-}
+        const cUser = await prisma.user.create({
+          data: {
+            firstName: profile.name?.givenName ?? "",
+            lastName: profile.name?.familyName ?? "",
+            email: email ?? `${profile.id}@facebook.com`,
+            password: await argon2.hash(secureOtp(32)),
+            facebookId: profile.id,
+          },
+          include: { curator: true },
+        });
+        return cb(undefined, cUser);
+      } catch (error) {
+        return cb(error);
+      }
+    }
+  );
+};
